56f0aaa2d2164b769f0f951debe97c3f5faf923c
Andi Rustianto <anditodev46@gmail.com>
2022-10-13 19:23:27 +0700

    [ADD] Cart Fragment

---
+++ app/src/main/java/com/anditorx/sepatu/ui/cart/CartActivity.java
@@ -0,0 +1,16 @@
+package com.anditorx.sepatu.ui.cart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.anditorx.sepatu.R;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
